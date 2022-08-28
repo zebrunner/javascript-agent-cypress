1bdc35349c588f079076c98b62ef17e18f63104d
Victor Mitskevich <vmitskevich@solvd.com>
2022-08-28 14:28:06 +0200

    Fix some linter warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,27 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
-      jest: true,
-      node: true
-    },
-    extends: "eslint:recommended",
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-      cy: true,
-      Cypress: true
-    },
-    parserOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-    },
-    rules: {
-      'no-console': 0,
-    },
-  };
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    cy: true,
+    Cypress: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 0,
+    indent: ['error', 2, { ignoreComments: true, SwitchCase: 1 }],
+    'max-len': 0,
+    'consistent-return': 0,
+    camelcase: 0,
+    'no-underscore-dangle': 0,
+  },
+};
